ac10b398ec4168b0410db358d92870f2ba784d29
BradHick <bradleyhrm@gmail.com>
2018-03-19 20:15:26 -0300

    creating the famous HelloWorld

---
+++ app/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class HelloWorld extends Component{
+ render(){
+   return(
+     <div className="My-class-from-react">
+      <h1>Hello World from React.js</h1>
+     </div>
+   )
+ }
+}
+
+
+export default HelloWorld
