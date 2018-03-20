8de34f9fdb8f3ec8acc744674badac4b6ccdd882
BradHick <bradleyhrm@gmail.com>
2018-03-20 00:31:28 -0300

    testing events

--- app/src/index.js
+++ app/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import HelloWorld from './HelloWorld'
 import PropsValidation from './PropsValidation';
+import Events from './Events';
 
 
 ReactDOM.render(
    //<HelloWorld txt ='Hello World from React.js'/>,
-   <PropsValidation name='Bradley' age='25' height='1,82'/>,
+   //<PropsValidation name='Bradley' age='25' height='1,82'/>,
+  <Events/>,
    document.getElementById(`root`)
 );
 
